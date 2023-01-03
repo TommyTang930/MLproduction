d4ce6de8c64f2c9aacc834eabd820db30c8120d8
唐国梁 <tgl@Tommy.local>
2023-01-03 13:01:58 +0800

    loan production update

--- Project_v2/train.py
+++ Project_v2/train.py
@@ -1,10 +1,2 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import KFold
-from sklearn.model_selection import GridSearchCV
 
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
-from sklearn.metrics import make_scorer
